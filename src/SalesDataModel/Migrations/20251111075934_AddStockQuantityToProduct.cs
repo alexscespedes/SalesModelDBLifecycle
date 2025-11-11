1f33bca28c4ae01473ff3b6bf7d1c9de02c9084a
Alexander Sencion Cespedes <alexandersenc99@gmail.com>
2025-11-11 04:03:10 -0400

    Add a New Migration with Simulate Rollback

---
+++ src/SalesDataModel/Migrations/20251111075934_AddStockQuantityToProduct.cs
@@ -0,0 +1,50 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace SalesDataModel.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddStockQuantityToProduct : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "StockQuantity",
+                table: "Products",
+                type: "int",
+                nullable: false,
+                defaultValue: 0);
+
+            migrationBuilder.UpdateData(
+                table: "Products",
+                keyColumn: "ProductId",
+                keyValue: 1,
+                column: "StockQuantity",
+                value: 0);
+
+            migrationBuilder.UpdateData(
+                table: "Products",
+                keyColumn: "ProductId",
+                keyValue: 2,
+                column: "StockQuantity",
+                value: 0);
+
+            migrationBuilder.UpdateData(
+                table: "Products",
+                keyColumn: "ProductId",
+                keyValue: 3,
+                column: "StockQuantity",
+                value: 0);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "StockQuantity",
+                table: "Products");
+        }
+    }
+}
--- src/SalesDataModel/Models/Product.cs
+++ src/SalesDataModel/Models/Product.cs
@@ -7,6 +7,7 @@ public class Product
     public int ProductId { get; set; }
     public string Name { get; set; } = string.Empty;
     public decimal Price { get; set; }
+    public int StockQuantity { get; set; }
 
     public ICollection<ProductCategory> ProductCategories { get; set; } = new List<ProductCategory>();
     public ICollection<OrderItem> OrderItems { get; set; } = new List<OrderItem>();
